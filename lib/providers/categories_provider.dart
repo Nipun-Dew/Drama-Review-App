712ac6b2ea8b64ff7599dc55912bcb8b699571ee
Denuke <ddnadhananja@gmail.com>
2021-07-18 02:13:47 +0530

    Add features

--- lib/providers/categories_provider.dart
+++ lib/providers/categories_provider.dart
@@ -19,6 +19,14 @@ class Categories with ChangeNotifier {
         id: "C4",
         title: "Documentries",
         imageUrl: "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"),
+    Category(
+        id: "C5",
+        title: "Cartoons",
+        imageUrl: "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"),
+    Category(
+        id: "C6",
+        title: "Educational",
+        imageUrl: "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"),
   ];
 
   List<Category> get items {
