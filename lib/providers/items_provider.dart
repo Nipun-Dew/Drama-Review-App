f57d40f3bd5b7c827234d07e9136011a742a3943
Denuke <ddnadhananja@gmail.com>
2021-07-18 01:57:45 +0530

    Add features

--- lib/providers/items_provider.dart
+++ lib/providers/items_provider.dart
@@ -11,10 +11,20 @@ class Items with ChangeNotifier {
         description:
             "Nadagamkarayo is a drama based on a village life. Sara, Kiriputha, Kawadiya, Sudda are four young boys who sell toddy for living. Master is the musician in that village. First half of the story goes with the Vesak drama which is produced by the Master. Kukula lakuna is the rowdy of that village who is determined to be a politician",
         cast: ["Sara - Sajith Anthoney", "Patali - Senali Fonseka", "Master - Prasannajith Abeysuriya", "Malan - Kokila Pawan Jayasooriya", "Kukula Lakuna - Giriraj Kaushalya"],
-        director: "Director",
-        producer: "Producer",
+        director: "Jayaprakash Sivagurunadan",
+        producer: "Chamara Samarawickrama  Saddhamangala Sooriyabandara",
         genre: Genre.Drama),
-    Item(id: "I2", category: "C1", title: "Thnamalvila Kollek", imageUrl: " ", description: "Thnamalvila kollek is a.....", cast: [], director: "Director", producer: "Producer", genre: Genre.Drama),
+    Item(
+        id: "I2",
+        category: "C1",
+        title: "Thnamalvila Kollek",
+        imageUrl: " ",
+        description:
+            "Thnamalvila kollek a life story of a village boy who is struggeling with many problems as a person in a rural area. This teledrama shows how win his life and make a succefull artist. THis teledrama shows calm and peacefullnes of the village as well as the dark side of the village life as poverty and how people are strugling to win their lives",
+        cast: ["Deeptha - Dhanuka Dilshan", "kithula - Xavier Kanishka", "koora - Nilanka Sepala Dahanayake", "Yashodhara - Yashoda Wimaladharma"],
+        director: "Roshan Ravindra",
+        producer: "Fahim Maujude",
+        genre: Genre.Drama),
     Item(id: "I3", category: "C2", title: "Agoha", imageUrl: " ", description: "Agohsa is a.....", cast: [], director: "Director", producer: "Producer", genre: Genre.Thriller),
     Item(id: "I4", category: "C3", title: "Suriya arana", imageUrl: " ", description: "Suriya arana is a.....", cast: [], director: "Director", producer: "Producer", genre: Genre.Thriller),
   ];
--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -16,6 +16,21 @@ class ItemDetailsScreen extends StatelessWidget {
     );
   }
 
+  Widget buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 200,
+      width: 300,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final itemData = Provider.of<Items>(context);
@@ -44,9 +59,31 @@ class ItemDetailsScreen extends StatelessWidget {
               child: Text(
                 selectedItem.description,
                 textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: "RobotoCondensed-Light", fontWeight: FontWeight.w500),
               ),
             ),
+            buildingSectionTitle(context, "Director"),
+            Text(
+              selectedItem.director,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontFamily: "RobotoCondensed-Light", fontWeight: FontWeight.w400, fontSize: 20),
+            ),
+            buildingSectionTitle(context, "Producer"),
+            Text(
+              selectedItem.producer,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontFamily: "RobotoCondensed-Light", fontWeight: FontWeight.w400, fontSize: 20),
+            ),
             buildingSectionTitle(context, "Cast"),
+            buildContainer(
+              ListView.builder(
+                itemCount: selectedItem.cast.length,
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10), child: Text(selectedItem.cast[index])),
+                ),
+              ),
+            ),
           ],
         ),
       ),
