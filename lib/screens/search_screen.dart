2a98bf01c9c5b110241da9c478843f84f4c786ec
Nipun Dewenarayane <ncdsomarathne@yahoo.com>
2021-07-25 14:25:49 +0530

    improve searchScreen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -6,32 +6,44 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: Container(
-              width: MediaQuery.of(context).size.width * 0.97,
-              margin: EdgeInsets.all(20),
-              child: TextField(
-                cursorHeight: 27,
-                style: TextStyle(decoration: TextDecoration.none),
-                decoration: InputDecoration(
-                    icon: InkWell(
-                        onTap: () => Navigator.pop(context),
-                        child: Icon(Icons.keyboard_arrow_down, size: 30,)),
-                    contentPadding:
-                        EdgeInsets.only(top: 1, left: 25, right: 20, bottom: 1),
-                    fillColor: Colors.grey[300],
-                    filled: true,
-                    prefixIcon: Icon(Icons.search_rounded),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            //width: MediaQuery.of(context).size.width * 0.2,
+            child: InkWell(
+                onTap: () => Navigator.pop(context),
+                child: Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Theme.of(context).primaryColor,
+                  size: 30,
+                )),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            margin: EdgeInsets.all(20),
+            child: TextField(
+              cursorHeight: 27,
+              style: TextStyle(decoration: TextDecoration.none),
+              decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.only(top: 1, left: 25, right: 20, bottom: 1),
+                  fillColor: Colors.grey[300],
+                  filled: true,
+                  prefixIcon: Icon(Icons.search_rounded),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
                     ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    labelText: "Search Dramas..."),
-              ),
-        ),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  labelText: "Search Dramas..."),
+            ),
+          ),
+        ]),
       ),
     );
   }
