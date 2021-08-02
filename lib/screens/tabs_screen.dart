808929c9754985e24d6d5474c743b5b32bd7c686
Nipun-Dew <e16360@eng.pdn.ac.lk>
2021-08-02 20:51:42 +0530

    when phone backBtn clicked in bottom tab view direct to home tab, if we are in the home tab it will close the app

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -31,11 +31,16 @@ class _TabScreenState extends State<TabScreen> {
     DrawerScreen()
   ];
 
-  void _onItemTapped(int index) {
+  Future<bool> _onItemTapped(int index) {
     setState(() {
       _selectIndex = index;
       Provider.of<Categories>(context, listen: false).categoryClick = false;
     });
+    return Future.value(false);
+  }
+
+  Future<bool> _exitApp() {
+    return Future.value(true);
   }
 
   @override
@@ -44,96 +49,106 @@ class _TabScreenState extends State<TabScreen> {
     double phoneWidth = MediaQuery.of(context).size.width;
     double phoneHeight = MediaQuery.of(context).size.height;
 
-    bool isCatClicked = Provider.of<Categories>(context, listen: true).categoryClicked;
+    bool isCatClicked =
+        Provider.of<Categories>(context, listen: true).categoryClicked;
 
-    return Scaffold(
-      //SingleChildScrollView(child: _currentTab[_selectIndex])
-      body: Container(
-        margin: EdgeInsets.only(top: statusBar),
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              primary: false,
-              floating: false,
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-              expandedHeight: phoneHeight * 0.12,
-              title: !isCatClicked ? Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      titles[_selectIndex],
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 26,
-                      ),
-                    ),
-                    CircleAvatar(
-                      radius: 15,
-                      backgroundColor: Theme.of(context).primaryColor,
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (_) => SearchScreen()));
-                        },
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.white,
+    return WillPopScope(
+      onWillPop:
+          _selectIndex == 0 && !isCatClicked ? _exitApp : () => _onItemTapped(0),
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.only(top: statusBar),
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                primary: false,
+                floating: false,
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                expandedHeight: phoneHeight * 0.12,
+                title: !isCatClicked
+                    ? Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              titles[_selectIndex],
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 26,
+                              ),
+                            ),
+                            CircleAvatar(
+                              radius: 15,
+                              backgroundColor: Theme.of(context).primaryColor,
+                              child: InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (_) => SearchScreen()));
+                                },
+                                child: Icon(
+                                  Icons.search,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
+                      )
+                    : Row(
+                        children: [
+                          InkWell(
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            onTap: () {
+                              Provider.of<Categories>(context, listen: false)
+                                  .categoryClick = false;
+                            },
+                          ),
+                          SizedBox(
+                            width: phoneWidth * 0.01,
+                          ),
+                          Text(
+                            Provider.of<Categories>(context).catTitle,
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.w900,
+                              fontSize: 23,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) =>
+                      Container(child: _currentTab[_selectIndex]),
+                  childCount: 1,
                 ),
-              ) : Row(
-                children: [
-                  InkWell(
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    onTap: () {
-                      Provider.of<Categories>(context, listen: false).categoryClick = false;
-                    },
-                  ),
-                  SizedBox(width: phoneWidth * 0.01,),
-                  Text(
-                    Provider.of<Categories>(context).catTitle,
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.w900,
-                      fontSize: 23,
-                    ),
-                  ),
-                ],
               ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) => Container(child: _currentTab[_selectIndex]),
-                childCount: 1,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.movie), label: "Trending"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: "Favourite"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: "Notifications"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.dehaze_rounded), label: "Drawer")
-        ],
-        currentIndex: _selectIndex,
-        onTap: _onItemTapped,
-        elevation: 15,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Theme.of(context).primaryColor,
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.movie), label: "Trending"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: "Favourite"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: "Notifications"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.dehaze_rounded), label: "Drawer")
+          ],
+          currentIndex: _selectIndex,
+          onTap: _onItemTapped,
+          elevation: 15,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Theme.of(context).primaryColor,
+        ),
       ),
     );
   }
