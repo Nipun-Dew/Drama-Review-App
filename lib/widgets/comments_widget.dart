a7c303f6c370e63657479b75bf0d4c90d7f3b313
Nipun-Dew <e16360@eng.pdn.ac.lk>
2021-08-03 14:03:47 +0530

    local changes

--- lib/widgets/comments_widget.dart
+++ lib/widgets/comments_widget.dart
@@ -5,34 +5,6 @@ class CommentItem extends StatelessWidget {
 
   CommentItem({required this.comment});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     // ignore: unused_local_variable
-
-//     return Card(
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(15),
-//       ),
-//       elevation: 4,
-//       margin: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
-//       child: ListTile(
-//         leading: Icon(
-//           Icons.face,
-//           size: 20,
-//         ),
-//         title: Text(
-//           comment,
-//           style: TextStyle(
-//             fontFamily: 'RobotoCondensed',
-//             fontSize: 18,
-//             fontWeight: FontWeight.normal,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
   @override
   Widget build(BuildContext context) {
     return Container(
