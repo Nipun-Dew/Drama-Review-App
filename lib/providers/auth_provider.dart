f985f89300dcedefac963ab6c0816ba5e901f8cc
nipun <ncdsomarathne@yahoo.com>
2021-08-09 14:47:22 +0530

    isAuth and getToken getters

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:jwt_decoder/jwt_decoder.dart';
 import '../models/http_exception.dart';
 
 import 'dart:convert';
@@ -9,6 +10,20 @@ class Auth with ChangeNotifier {
   String token = "";
   String expireDate = "";
   String userId = "";
+
+  bool get isAuth {
+    if(token != "" && !JwtDecoder.isExpired(token)) {
+      return true;
+    }
+    return false;
+  }
+
+  String? get getToken {
+    if(token != "" && !JwtDecoder.isExpired(token)) {
+      return token;
+    }
+    return null;
+  }
 
   Future<void> signup(
       String username, String useremail, String password) async {
@@ -43,7 +58,8 @@ class Auth with ChangeNotifier {
         ), headers: {"content-type" : "application/json"});
     if (responce.statusCode == 200) {
       token = json.decode(responce.body)['jwt'];
-      print(token);
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+      print(decodedToken);
     } else if (responce.statusCode == 400) {
       print(responce.body);
       throw HttpException(responce.body);
