fd394675391ad526b86a9a6a1ad19c7feca24102
nipun <ncdsomarathne@yahoo.com>
2021-08-12 01:42:49 +0530

    add auto login function

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/http_exception.dart';
 
 import 'dart:convert';
@@ -54,6 +57,14 @@ class Auth with ChangeNotifier {
       expireTime = JwtDecoder.getExpirationDate(token);
       autoLogout();
       notifyListeners();
+
+      final prefs = await SharedPreferences.getInstance();
+      String data = json.encode({
+        "token": token,
+        "expireTime": expireTime!.toIso8601String(),
+      });
+      prefs.setString("dramaUser", data);
+
     } else if (response.statusCode == 400) {
       print(response.body);
       throw HttpException(response.body);
@@ -63,10 +74,16 @@ class Auth with ChangeNotifier {
     }
   }
 
-  void logout() {
+  Future<void> logout() async {
     token = "";
     expireTime = DateTime.now();
     userId = "";
+    if(authTimer != null) {
+      authTimer!.cancel();
+      authTimer = null;
+    }
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
     notifyListeners();
   }
 
@@ -77,6 +94,27 @@ class Auth with ChangeNotifier {
     var timeDuration = expireTime!.difference(DateTime.now()).inSeconds;
     print("$timeDuration  $expireTime");
     authTimer = Timer(Duration(seconds: timeDuration), logout);
+  }
 
+  Future<bool> autoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    print("try");
+    print(prefs.containsKey("dramaUser"));
+    if(!prefs.containsKey("dramaUser")) {
+      return false;
+    }
+    final fetchToken = json.decode(prefs.getString('dramaUser')!)['token'];
+    final fetchExpTimeString = json.decode(prefs.getString('dramaUser')!)['expireTime'];
+
+    final fetchExp = DateTime.parse(fetchExpTimeString);
+    print(fetchExp);
+    
+    if(fetchExp.isBefore(DateTime.now())) {
+      return false;
+    }
+    token = fetchToken;
+    expireTime = fetchExp;
+    notifyListeners();
+    return true;
   }
 }
