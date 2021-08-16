bc039df9a44b121faa1c57f5cdebbf485f42689c
Denuke <ddnadhananja@gmail.com>
2021-08-17 01:13:46 +0530

    Edited Items_Screen

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -88,11 +88,11 @@ class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final itemData = Provider.of<Items>(context);
-    final items = itemData.items;
+    // final itemData = Provider.of<Items>(context);
+    // final items = itemData.items;
 
-    final castData = Provider.of<Casts>(context);
-    final itemsCasts = castData.items;
+    // final castData = Provider.of<Casts>(context);
+    // final itemsCasts = castData.items;
 
     //final selectedItem = items.firstWhere((item) => item.id == widget.id);
 
@@ -107,26 +107,26 @@ class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
     final selectedRoles = [];
 
     wholeItem.cast.forEach((item) {
-      itemsCasts.forEach((role) {
-        if (role.name.toString() == item["starID"].toString()) {
-          selectedCast.add({"name": item["starID"].toString(), "roleName": item["role"].toString(), "imageUrl": role.imageUrls[0]});
-        }
+      selectedCast.add({
+        "name": item['starID'],
+        "roleName": item['role'],
+        "imageUrl": item['imageUrl'],
       });
     });
 
     wholeItem.directors.forEach((item) {
-      itemsCasts.forEach((role) {
-        if (role.name.toString() == item["starID"].toString()) {
-          selectedRoles.add({"name": item["starID"].toString(), "roleName": item["role"].toString(), "imageUrl": role.imageUrls[0]});
-        }
+      selectedRoles.add({
+        "name": item['starID'],
+        "roleName": item['role'],
+        "imageUrl": item['imageUrl'],
       });
     });
 
     wholeItem.producers.forEach((item) {
-      itemsCasts.forEach((role) {
-        if (role.name.toString() == item["starID"].toString()) {
-          selectedRoles.add({"name": item["starID"].toString(), "roleName": item["role"].toString(), "imageUrl": role.imageUrls[0]});
-        }
+      selectedRoles.add({
+        "name": item['starID'],
+        "roleName": item['role'],
+        "imageUrl": item['imageUrl'],
       });
     });
 
