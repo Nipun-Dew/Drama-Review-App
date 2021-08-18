a55dec06daded14108bbde6bdbdc3c2c17a4669e
Denuke <ddnadhananja@gmail.com>
2021-08-18 16:24:42 +0530

    Added refreshIndicator to category items Screen

--- lib/screens/category_items_screen.dart
+++ lib/screens/category_items_screen.dart
@@ -45,6 +45,28 @@ class _CategoryItemScreenState extends State<CategoryItemScreen> {
     super.initState();
   }
 
+  Future<void> _refreshItems(String id) async {
+    // await Provider.of<Items>(context).getTeledramas();
+
+    print("Refreshing");
+
+    if (id.toString() == 'teledrama') {
+      await Provider.of<Items>(context, listen: false).getTeledramas().then((_) {});
+    }
+    if (id.toString() == 'web-series') {
+      await Provider.of<Items>(context, listen: false).getWebSeries().then((_) {});
+    }
+    if (id.toString() == 'movie') {
+      await Provider.of<Items>(context, listen: false).getMovies().then((_) {});
+    }
+    if (id.toString() == 'short-movie') {
+      await Provider.of<Items>(context, listen: false).getShortMovies().then((_) {});
+    }
+    if (id.toString() == 'mini-series') {
+      await Provider.of<Items>(context, listen: false).getMiniSeries().then((_) {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var displayItems;
@@ -71,20 +93,23 @@ class _CategoryItemScreenState extends State<CategoryItemScreen> {
               child: CircularProgressIndicator(),
             ),
           )
-        : Column(
-            children: [
-              ...displayItems.map((item) {
-                return ItemWidget(
-                  id: item.id,
-                  title: item.title,
-                  imageUrls: item.imageUrls[0],
-                  category: item.category,
-                  trailerVideoUrl: item.youtubeURL,
-                  genres: item.genres,
-                  wholeItem: item,
-                );
-              })
-            ],
+        : RefreshIndicator(
+            onRefresh: () => _refreshItems(widget.id.toString()),
+            child: Column(
+              children: [
+                ...displayItems.map((item) {
+                  return ItemWidget(
+                    id: item.id,
+                    title: item.title,
+                    imageUrls: item.imageUrls[0],
+                    category: item.category,
+                    trailerVideoUrl: item.youtubeURL,
+                    genres: item.genres,
+                    wholeItem: item,
+                  );
+                })
+              ],
+            ),
           );
   }
 }
